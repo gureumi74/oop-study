73ad46099e456eeddabb70fe61c028ae8b8464fa
하늘 <gureumi74@naver.com>
2023-06-01 20:06:09 +0900

    test: 두 수를 더해서 9이하 이도록 테스트 중

--- src/test/java/com/example/oopstudy/opgame/service/OpGameTest.java
+++ src/test/java/com/example/oopstudy/opgame/service/OpGameTest.java
@@ -19,4 +19,18 @@ class OpGameTest {
         assertEquals("한번 더 해보자", game.getCheeringUpMsg());
         assertEquals(true, game.isAnswer(2));
     }
+
+    // 10 미만의 랜덤한 수가 잘 나오는지 확인
+    @Test
+    public void testRandom() {
+        System.out.println((int)(Math.random() * 10));
+    }
+
+    // 둘을 더했을 때 10 미만인지 확인
+    @Test
+    public void testSumMax() {
+        int a = 0;
+        int b = 0;
+        assertTrue(a + b < 10);
+    }
 }
