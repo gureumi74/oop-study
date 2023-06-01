efb5f5ff1b93ae0358d2db1946dfadcff21d9d03
하늘 <gureumi74@naver.com>
2023-06-01 20:17:56 +0900

    feat: 게임 규칙을 인터페이스로 분리중(test x)

--- src/main/java/com/example/oopstudy/opgame/service/OpGame.java
+++ src/main/java/com/example/oopstudy/opgame/service/OpGame.java
@@ -2,29 +2,30 @@ package com.example.oopstudy.opgame.service;
 
 public class OpGame {
     int remainingAnswer;
-    int a, b;
     String cheeringUpMsg = "한번 더 해보자";
+    IOperator op;
 
-    public OpGame(PlusOperatorLevelOne plusOperatorLevelOne) {
-
+    public OpGame(IOperator op) {
+        this.op = op;
     }
 
     // 문제 만들기
     public void makeQuestion() {
         // 3번의 기회
         remainingAnswer = 3;
-        a = 1;
-        b = 1;
+        op.setA(1);
+        op.setB(1);
     }
 
     public void makeQuestion(int max) {
-        a = (int)(Math.random() * max);
-        b = (int)(Math.random() * (max - a));
+//        a = (int)(Math.random() * max);
+//        b = (int)(Math.random() * (max - a));
+        op.generateQuestion(max);
     }
 
     // 문제 가져오기
     public String getQuestion() {
-        return String.format("%d + %d = ", a, b);
+        return op.getQuestionMsg();
     }
 
     // 몇번 기회가 남았는지 확인
@@ -35,7 +36,8 @@ public class OpGame {
     // 정답인지 확인
     public boolean isAnswer(int answer) {
         remainingAnswer--;
-        return (a + b) == answer;
+//        return (a + b) == answer;
+        return op.isEqueals(answer);
     }
 
     // 기회를 다 날렸을 경우
--- src/main/java/com/example/oopstudy/opgame/service/PlusOperatorLevelOne.java
+++ src/main/java/com/example/oopstudy/opgame/service/PlusOperatorLevelOne.java
@@ -1,4 +1,31 @@
 package com.example.oopstudy.opgame.service;
 
 public class PlusOperatorLevelOne {
+    public class PlusOpertorLevelOne implements IOperator {
+
+        @Override
+        public void setA(int a) {
+
+        }
+
+        @Override
+        public void setB(int b) {
+
+        }
+
+        @Override
+        public void generateQuestion(int max) {
+
+        }
+
+        @Override
+        public String getQuestionMsg() {
+            return null;
+        }
+
+        @Override
+        public boolean isEqueals(int answer) {
+            return false;
+        }
+    }
 }
