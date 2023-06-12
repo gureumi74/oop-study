a203f608285d85fda759b8df6a9c7fd7248bb6c4
하늘 <gureumi74@naver.com>
2023-06-12 21:35:25 +0900

    feat: id를 중복되지 않도록 설정

--- src/main/java/com/example/oopstudy/opgame/model/DBMain.java
+++ src/main/java/com/example/oopstudy/opgame/model/DBMain.java
@@ -10,20 +10,23 @@ public class DBMain extends BaseDAO {
     }
 
     private void initPerson() {
-        getConn();
+        String sql =
+                "create table person (id integer primary key autoincrement, name string)";
         try {
+            getConn();
             // SQL문을 실행하기 위한 statement 객체를 생성
             smt = conn.createStatement();
+            smt.setQueryTimeout(30); // set timeout to 30 sec.
             // insert, update, delete 등의 sql문을 실행하고 영향을 받은 행의 수를 반환
             // world.db에 차례대로 쿼리를 보낸다.
             // world.db에 'person'이라는 Table이 존재하면 일단 지우고
             smt.executeUpdate("drop  table if exists person");
             // id와 name column을 가지고 있는 'person' table 생성
-            smt.executeUpdate("create table person (id integer, name string)");
+            smt.executeUpdate(sql);
             // id = 1, name = 'leo' 데이터 삽입
-            smt.executeUpdate("insert into person values(1, 'leo')");
+            smt.executeUpdate("insert into person(name) values('leo')");
             // id = 2, name = 'yui' 데이터 삽입
-            smt.executeUpdate("insert into person values(2, 'yui')");
+            smt.executeUpdate("insert into person(name) values('yui')");
             // 'person' table 조회해서 rs에 넘겨준다.
             rs = smt.executeQuery("select * from person");
 
