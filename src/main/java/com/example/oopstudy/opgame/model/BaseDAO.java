5a8371de0b9f2ce8a38e5773f1c9b36ae79c10f7
하늘 <gureumi74@naver.com>
2023-06-12 21:13:44 +0900

    feat: 중복되는 코드 BaseDao 생성으로 리팩토링

--- src/main/java/com/example/oopstudy/opgame/model/BaseDAO.java
+++ src/main/java/com/example/oopstudy/opgame/model/BaseDAO.java
@@ -5,19 +5,25 @@ import java.util.Map;
 import static java.lang.System.getenv;
 
 public class BaseDAO {
+    // 하나의 논리적 데이터베이스 트랜잭션을 나타내는 클래스
     protected Connection conn = null;
+    // sql 구문의 실행을 담당
     protected Statement smt = null;
+    // 미리 준비된 SQL 문을 실행하기 위한 PreparedStatement 객체를 생성
     protected PreparedStatement psmt = null;
+    // 데이터베이스 질의를 통해서 얻어진 데이터 결과값의 행을 나타내는 객체
     protected ResultSet rs = null;
 
     protected void getConn() {
         // 호스트 정보를 변수로 설정
+
         Map<String, String> env = getenv();
         String dbHost = env.get("DB_HOST");
         String url = dbHost;
         String user = "";
         String pw = "";
 
+        // 드라이버 리스트를 저장하고 요청된 URL에 해당하는 드라이버를 애플리케이션에 제공(world.db에 연결)
         try {
             conn = DriverManager.getConnection(url);
         } catch (SQLException e) {
--- src/main/java/com/example/oopstudy/opgame/model/DBMain.java
+++ src/main/java/com/example/oopstudy/opgame/model/DBMain.java
@@ -3,30 +3,17 @@ package com.example.oopstudy.opgame.model;
 import javax.xml.namespace.QName;
 import java.sql.*;
 
-public class DBMain {
+public class DBMain extends BaseDAO {
     public static void main(String[] args) {
-        initPerson();
+        DBMain dbMain = new DBMain();
+        dbMain.initPerson();
     }
 
-    private static void initPerson() {
-        String sql = "select code, name from country order by code, name";
-        // 하나의 논리적 데이터베이스 트랜잭션을 나타내는 클래스
-        Connection conn = null;
-        // sql 구문의 실행을 담당
-        Statement smt = null;
-        // 미리 준비된 SQL 문을 실행하기 위한 PreparedStatement 객체를 생성
-        PreparedStatement psmt = null;
-        // 데이터베이스 질의를 통해서 얻어진 데이터 결과값의 행을 나타내는 객체
-        ResultSet rs = null;
+    private void initPerson() {
+        getConn();
         try {
-            // 드라이버 리스트를 저장하고 요청된 URL에 해당하는 드라이버를 애플리케이션에 제공(world.db에 연결)
-            conn = DriverManager.getConnection("jdbc:sqlite:world.db");
             // SQL문을 실행하기 위한 statement 객체를 생성
             smt = conn.createStatement();
-            smt.setQueryTimeout(30); // set timeout to 30 sec
-            // select 문을 실행하고 결과를 ResultSet 객체로 반환
-//            rs = psmt.executeQuery();
-
             // insert, update, delete 등의 sql문을 실행하고 영향을 받은 행의 수를 반환
             // world.db에 차례대로 쿼리를 보낸다.
             // world.db에 'person'이라는 Table이 존재하면 일단 지우고
@@ -50,22 +37,24 @@ public class DBMain {
         } catch (SQLException e) {
             System.err.println(e.getMessage());
         } finally {
-            try {
-                // 사용한 JDBC 객체 (ResultSet, Statement, Connection 등)을 닫고 관련된 리소스를 해제한다.
-                if (rs != null) rs.close();
-            } catch (Exception e) {
-                e.printStackTrace();
+            close();
+        }
+    }
+    private void getCodeName() {
+        String sql = "select code, name from country order by code, name";
+        try {
+            getConn();
+
+            psmt = conn.prepareStatement(sql);
+            rs = psmt.executeQuery();
+            while (rs.next()) {
+                System.out.println(rs.getString("code") + " ");
+                System.out.println(rs.getString("name"));
             }
-            try {
-                if (psmt != null) psmt.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            try {
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close();
         }
     }
 }
